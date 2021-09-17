3e05265480f88f5b36d3d51bbe0ba14f81745d03
Tom Zhang <tom.zhang989@gmail.com>
2021-09-17 17:38:44 -0400

    created the function body

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-
+// Sum all the values from fromN up to toN
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  if (fromN === toN) {
+    return fromN;
+  }
+  return (fromN + (sum(fromN+1, toN)));
+};
 
 module.exports = sum;
